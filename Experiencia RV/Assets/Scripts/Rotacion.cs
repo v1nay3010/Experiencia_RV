1d7258983a4e78dc002b84a13a356ff29c1ab116
JUAN-JOSE-CADENA <69523656+JUAN-JOSE-CADENA@users.noreply.github.com>
2021-10-28 20:02:19 -0500

    Codigo humilde rotacion

--- Experiencia RV/Assets/Scripts/Rotacion.cs
+++ Experiencia RV/Assets/Scripts/Rotacion.cs
@@ -7,12 +7,12 @@ public class Rotacion : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        transform.Rotate(new Vector3(0f, 30f, 0f) * Time.deltaTime);
     }
 }
